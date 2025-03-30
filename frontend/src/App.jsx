65665f4e27b26612c9641a596614f8e73dbc4611
Aayush Acharya <151422854+aayush4515@users.noreply.github.com>
2025-03-30 05:14:14 +0000

    Updated route URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
     e.preventDefault();
 
     try{
-      const res = await fetch("https://tile-puzzle-solver.onrender.com", {
+      const res = await fetch("https://tile-puzzle-solver.onrender.com/solve", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
